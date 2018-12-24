6cda61660c8dad50b2045d07f35dd05c93813283
toh12345 <44148216+toh12345@users.noreply.github.com>
2018-12-24 14:54:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/henry/lab42database/AddActivity.java
@@ -0,0 +1,13 @@
+package com.example.henry.lab42database;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+}
